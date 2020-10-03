f8a7d738131e6f916174ccae7f3d32b710ca232f
pranay101 <pranayprajapati101@gmail.com>
2020-10-03 09:52:59 +0000

    implemented grayscale

--- helpers.c
+++ helpers.c
@@ -3,6 +3,18 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int average;
+    for(int i=0; i < height; i++)
+    {
+        for(int j=0; j < width ; j++)
+        {
+            average = (image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen )/ 3;
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
