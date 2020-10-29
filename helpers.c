565fca580e255e7fee668365b7d593de235c2f44
pranay101 <pranayprajapati101@gmail.com>
2020-10-29 17:05:18 +0000

    final commit

--- helpers.c
+++ helpers.c
@@ -1,7 +1,20 @@
 #include "helpers.h"
-
+#include <math.h>
+#include <stdlib.h>
 
 // some usefull functions
+
+int check(int a)
+{
+    if (a>255)
+    {
+        return a=255;
+    }
+    else
+    {
+        return a;
+    }
+}
 
 void swap(RGBTRIPLE *add1, RGBTRIPLE *add2)
     {
@@ -27,6 +40,14 @@ void swap(RGBTRIPLE *add1, RGBTRIPLE *add2)
 //     image[row][col].rgbtBlue= averageblue/count;
 //     image[row][col].rgbtGreen averagegreen/count;
 // }
+
+
+// compute edges
+int compute_edge(int a , int b)
+{
+  return sqrt((a*a + b*b));
+}
+
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -119,62 +140,76 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+
+
+
+
 // Detect edges
+
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-  int Gx[9]={-1,0,1,-2,0,2,-1,0,1};
-  int Gy[9]={-1,-2,-1,0,0,0,1,2,1};
-  int startRow,endRow,startColumn,endColumn;
-  int traverse_matrix=0;
-  struct sumGx
-  {
-    int red=0;
-    int green=0;
-    int blue =0;
-  };
-  struct sumGy
-  {
-    int red=0;
-    int green=0;
-    int blue =0;
-  }
+    float GxRed, GxBlue, GxGreen;
+    float GyRed, GyBlue, GyGreen;
+    float X,Y;
+    //create a temporary image 2d array to not change calculation after every loop
+    RGBTRIPLE temp[height][width];
 
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
+    for (int i = 0; i < width; i++)
     {
-        traverse_matrix=0;
-        sumGx=0;
-        sumGy=0;
-        if(i - 1 < 0)
-          startRow = i;
-        else
-          startRow = i-1;
-        if(i + 1 >= height)
-          endRow = i;
-        else
-          endRow = i+1;
-        if(j - 1 < 0)
-          startColumn = j;
-        else
-          startColumn = j-1;
-        if(j + 1 >= width)
-          endColumn = j;
-        else
-          endColumn = j+1;
+        for (int j = 0; j < height; j++)
+        {
 
-          for(int k=startRow; k<=endRow; k++)
-          {
-            for(int l= startColumn; l<=endColumn; l++)
+            GyBlue = 0.0;
+            GyGreen = 0.0;
+            GyRed = 0.0;
+            GxBlue = 0.0;
+            GxGreen = 0.0;
+            GxRed = 0.0;
+
+            // sums values of the pixel and 8 neighboring ones after applying a modifier, skips iteration if it goes outside the pic
+            for (int k = -1; k < 2; k++)
             {
-                sumGx.green += image[k][l];
-                sumGx.green += image[k][l];
-                sumGx.green += image[k][l];
-            }
-          }
+                if (j + k < 0 || j + k > height - 1)
+                {
+                    continue;
+                }
 
+                for (int h = -1; h < 2; h++)
+                {
+                    if (i + h < 0 || i + h > width - 1)
+                    {
+                        continue;
+                    }
+
+                    // calculates modifiers for vertical and horizantal borders
+                    X = (k + 1 * k - k * abs(h));
+                    Y = (h + 1 * h - h * abs(k));
+
+                    GxBlue += image[j + k][i + h].rgbtBlue * X;
+                    GxGreen += image[j + k][i + h].rgbtGreen * X;
+                    GxRed += image[j + k][i + h].rgbtRed * X;
+
+                    GyBlue += image[j + k][i + h].rgbtBlue * Y;
+                    GyGreen += image[j + k][i + h].rgbtGreen * Y;
+                    GyRed += image[j + k][i + h].rgbtRed * Y;
+                }
+            }
+
+            // obtains the final values of the pixels combining X and Y calculations
+            temp[j][i].rgbtBlue = check(round(sqrt(GxBlue * GxBlue + GyBlue * GyBlue)));
+            temp[j][i].rgbtGreen = check(round(sqrt(GxGreen * GxGreen + GyGreen * GyGreen)));
+            temp[j][i].rgbtRed = check(round(sqrt(GxRed * GxRed + GyRed * GyRed)));
+        }
     }
-  }
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            image[j][i].rgbtBlue = temp[j][i].rgbtBlue;
+            image[j][i].rgbtGreen = temp[j][i].rgbtGreen;
+            image[j][i].rgbtRed = temp[j][i].rgbtRed;
+        }
+    }
     return;
 }
 
